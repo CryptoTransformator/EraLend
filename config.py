5e30807f6dd0d9336fce3a75d2e6190fbbebc936
CryptoTransformator <kachanseva@gmail.com>
2023-08-28 11:37:50 +0100

    config ideal settings

--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 gas_treshold = 30
 
 # Target trading volume in USD.
-desired_volume_doll = 5000
+desired_volume_doll = 10000
 
 # True to randomize wallet selection.
 randomize_wallets = True
 
 # Multiplier for gas limit; 1 for no adjustment.
-gas_adjust = 0.9
+gas_adjust = 1
 
 # Min sleep time in seconds between transactions.
-sleep_time_from = 10
+sleep_time_from = 20
 
 # Max sleep time in seconds between transactions.
-sleep_time_to = 15
+sleep_time_to = 20
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 import json
 import random
 import os
-
+from loguru import logger
 
 current_working_directory = os.getcwd()
 web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/zksync_era'))
@@ -17,6 +17,11 @@ private_keys = import_from_file(os.path.join(current_working_directory, 'private
 if randomize_wallets:
     random.seed(32)
     random.shuffle(private_keys)
+
+
+logger.success(f'Created by CryptoTransformator')
+logger.info(f'https://github.com/CryptoTransformator')
+logger.info(f'https://t.me/gori_levi')
 
 for private_key in private_keys:
     MakeVolumeEraLend(web3, abi, private_key, desired_volume_doll, gas_treshold, gas_adjust, sleep_time_from, sleep_time_to)
